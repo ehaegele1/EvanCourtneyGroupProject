e2023dfcd9d02316fef195553340f65234cdad24
Courtney Zavaleta <courtneyzavaleta@gmail.com>
2020-03-23 15:09:06 +0000

    blog schema set up

---
+++ controllers/horror.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const Blog = require('../models/horror.js');
+
+
+
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+app.use(express.json());
+app.use(express.static('public'));
+
+
+//CONTROLLERS
+const horrorController = require('./controllers/horror.js');
+app.use('/horror', horrorController);
 
 app.get('/', (req, res) => {
   res.send("Hello World");
@@ -7,7 +16,10 @@ app.get('/', (req, res) => {
 
 
 
-
+mongoose.connect('mongodb://localhost:27017/meancrud', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once('open', ()=>{
+    console.log('connected to mongoose...');
+});
 
 app.listen(3000, ()=>{
 	console.log('listening, yo!');
